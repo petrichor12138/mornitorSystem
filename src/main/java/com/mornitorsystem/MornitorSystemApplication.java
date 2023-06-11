6585af01889ff93b25d438aae3687be7605cac41
Bruce Yang <yf@Bruces-MacBook.local>
2023-06-11 13:18:54 +0800

    first upload

---
+++ src/main/java/com/mornitorsystem/MornitorSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mornitorsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MornitorSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MornitorSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mornitorsystem/MornitorSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mornitorsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MornitorSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
